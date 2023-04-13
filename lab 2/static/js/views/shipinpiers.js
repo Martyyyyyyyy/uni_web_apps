df096264b529f52990897ac20f43fc8101bd6519
Martin Fesenko <Mfesenko2003@gmail.com>
2023-04-13 11:21:13 +0300

    lab 2 completed

---
+++ lab 2/static/js/views/shipinpiers.js
@@ -0,0 +1,113 @@
+'use strict'
+
+const shipModel = new Ship();
+const pierModel = new Pier();
+const shipInPiersModel = new shipInPiers() // eslint-disable-line no-undef
+
+function initSelectTag(tagId, model) {
+  let collection = model.Select();
+  let selectTag = document.getElementById(tagId);
+  selectTag.innerHTML = ''; // Clear the select tag before appending options
+  for(let i = 0; i < collection.length; ++i) {
+  let optionTag = document.createElement('option');
+  optionTag.setAttribute('value', collection[i].id);
+  optionTag.innerText = collection[i].name;
+  selectTag.appendChild(optionTag);
+  }
+}
+
+function initAddForm() {
+    initSelectTag('ship', shipModel);
+    initSelectTag('pier', pierModel);
+
+    const form = window.document.querySelector('#shipinpiers-add-form')
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        const shipInPiersData = {}
+        formData.forEach((value, key) => {
+            shipInPiersData[key] = value
+        })
+
+        shipInPiersModel.Create(shipInPiersData)
+
+        e.target.reset()
+    })
+}
+
+function initList() {
+    window.jQuery('#shipinpiers-list').DataTable({
+        data: shipInPiersModel.Select(),
+        columns: [
+            { title: 'ID', data: 'id' },
+            { title: 'Ship', data: 'ship' },
+            { title: 'Pier', data: 'pier' },
+            { title: 'Delete', data: '' },
+             {title: 'Edit', data: 'id'}
+        ],
+        columnDefs: [
+            {
+                render: function(data, type, row) {
+                    let ship = shipModel.FindById(data);
+                    return ship.name;
+                },
+                targets: 1
+            },
+            {
+                render: function(data, type, row) {
+                    let pier = pierModel.FindById(data);
+                    return pier.name;
+                },
+                targets: 2
+            },
+            {
+                render: function(data, type, row) {
+                    return '<button type="button" value="delete" onclick="deleteItem(this)">Delete</button>';
+                    
+                },
+                targets: 3
+            },
+            {
+              render: function(data, type, row) {
+               
+                    return '<button type="button" onclick="updateUser(this)"> Edit </button>'
+                  
+            },
+              targets: 4
+          }
+        ]
+    })
+}
+
+function initListEvents() {
+    document.addEventListener('shipinpierssListDataChanged', function(e) {
+        const dataTable = window.jQuery('#shipinpiers-list').DataTable()
+
+        dataTable.clear()
+        dataTable.rows.add(e.detail)
+        dataTable.draw()
+    }, false)
+}
+
+function deleteItem(e) {
+    let row = e.parentNode.parentNode;
+    let id = row.getElementsByTagName('td')[0].innerText;
+    row.remove();
+    shipInPiersModel.Delete(id);
+    alert("deletUser")
+}
+
+function updateUser(e){
+   
+    let row = e.parentNode.parentNode;
+    let id = row.getElementsByTagName('td')[0].innerText;
+    shipInPiersModel.Edit(shipInPiersModel,row);
+ }
+
+window.addEventListener('DOMContentLoaded', e => {
+    initAddForm()
+    initList()
+    initListEvents()
+})
